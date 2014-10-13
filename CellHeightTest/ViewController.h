fdeb26998dcd18cda5a250d5804d8664bf0e6ad6
Ben Packard <ben.packard@gmail.com>
2014-10-13 15:59:28 -0400

    Initial Commit

---
+++ CellHeightTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CellHeightTest
+//
+//  Created by Ben Packard on 10/13/14.
+//  Copyright (c) 2014 Ben Packard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CellHeightTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CellHeightTest
+//
+//  Created by Ben Packard on 10/13/14.
+//  Copyright (c) 2014 Ben Packard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
